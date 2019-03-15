894aef3c3dc020c02dba867baef0033165732296
Chris Styles <christopherstyles@gmail.com>
2019-03-14 23:37:34 -0700

    Remove unnecessary before hook

--- spec/features/bookmark_feature_spec.rb
+++ spec/features/bookmark_feature_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Bookmark management', type: :feature do
   let(:user) { create(:user) }
 
-  before do
-    visit root_path(as: user)
-  end
-
   it 'enables me to create bookmarks' do
-    visit '/bookmarks/new'
+    visit new_bookmark_path(as: user)
     fill_in 'Url', with: Faker::Internet.url
     click_button 'Create Bookmark'
     expect(page).to have_text('Bookmark was successfully created.')

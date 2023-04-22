8e570f92c1a8eae0e2c2de10de28776e5212c641
Chris Styles <christopherstyles@users.noreply.github.com>
2023-04-22 13:38:49 -0700

    Update Rails to 6.1.7.3

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_013_070_958) do
+ActiveRecord::Schema.define(version: 2023_04_22_202722) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"

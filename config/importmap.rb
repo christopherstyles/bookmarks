575ef3113add4ca0932a411ac06435aedec84c58
Chris Styles <christopherstyles@users.noreply.github.com>
2023-04-22 17:17:51 -0700

    Update turbo-rails config

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,7 @@ pin "application", preload: true
 pin "stimulus-reveal-controller", to: "https://ga.jspm.io/npm:stimulus-reveal-controller@4.1.0/dist/stimulus-reveal-controller.mjs"
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "feather-icons", to: "https://ga.jspm.io/npm:feather-icons@4.29.0/dist/feather.js"
 pin "simplemde", to: "https://cdn.jsdelivr.net/npm/simplemde@1.11.2/src/js/simplemde.js"
 pin_all_from "app/javascript/controllers", under: "controllers"

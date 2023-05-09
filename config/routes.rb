afb8fb36668429ff8bd145dd5b7f6dfb74a17142
Chris Styles <christopherstyles@users.noreply.github.com>
2023-05-09 16:30:40 -0700

    Mount letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   resources :bookmarks do
     get :search, on: :collection
   end

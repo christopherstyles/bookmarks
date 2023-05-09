158e8b8be0b9bc640b46f9537bf84fff9d641d5c
Chris Styles <christopherstyles@users.noreply.github.com>
2023-05-09 16:33:50 -0700

    Use EasyMDE over abandoned SimpleMDE

--- app/javascript/controllers/markdown_controller.js
+++ app/javascript/controllers/markdown_controller.js
@@ -3,19 +3,54 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['textarea'];
 
+  static values = {
+    options: { type: Object, default: {} },
+  };
+
   connect() {
-    this.editor = new SimpleMDE({
-      autoDownloadFontAwesome: true,
-      element: this.textareaTarget,
-      indentWithTabs: false,
-      renderingConfig: {
-        codeSyntaxHighlighting: true,
-      },
+    this.editor = new EasyMDE({
+      ...this.defaultOptions,
+      ...this.optionsValue,
     });
   }
 
   disconnect() {
     this.editor.toTextArea();
     this.editor = null;
+  }
+
+  updateContent({ detail: { content } }) {
+    console.log('updateContent', content);
+    this.editor.value(content);
+  }
+
+  /**
+   * Generate a random string of letters; used to generate a unique ID
+   * for each editor.
+   */
+  generateId(length = 6) {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+  get defaultOptions() {
+    return {
+      autoDownloadFontAwesome: true,
+      autoSave: true,
+      element: this.textareaTarget,
+      indentWithTabs: false,
+      renderingConfig: {
+        codeSyntaxHighlighting: true,
+      },
+      uniqueId: `notes-${this.generateId(6)}`,
+    };
   }
 }

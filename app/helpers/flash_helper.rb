b9c160c1e6423bdb110a00fabb6a1eb91f84cc8b
Chris Styles <christopherstyles@users.noreply.github.com>
2023-05-09 16:32:56 -0700

    Update flash helper

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -3,9 +3,7 @@
 module FlashHelper
   def classes_for_flash(flash_type)
     case flash_type.to_sym
-    when :error
-      "bg-red-100 text-red-700"
-    when :alert
+    when :error, :alert
       "bg-red-100 text-red-700"
     else
       "bg-blue-100 text-blue-700"

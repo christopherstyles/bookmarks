a8f617e0584c515acf159b7c0cc19027444d3480
Chris Styles <christopherstyles@users.noreply.github.com>
2023-05-09 16:30:55 -0700

    Pin turndown and turndown-plugin-gfm

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "feather-icons", to: "https://ga.jspm.io/npm:feather-icons@4.29.0/dist/feather.js"
-pin "simplemde", to: "https://cdn.jsdelivr.net/npm/simplemde@1.11.2/src/js/simplemde.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.2/dist/js/tom-select.complete.js"
 pin "lodash-es", to: "https://ga.jspm.io/npm:lodash-es@4.17.21/lodash.js"
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "turndown", to: "https://ga.jspm.io/npm:turndown@7.1.2/lib/turndown.browser.es.js"
+pin "turndown-plugin-gfm", to: "https://ga.jspm.io/npm:turndown-plugin-gfm@1.0.2/lib/turndown-plugin-gfm.cjs.js"
